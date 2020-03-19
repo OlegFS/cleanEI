1dfe07f02f3e3b1bf33192298312fc8b61f99f4e
Oleg Vinogradov <ovinogradov@fenrir.mvl6.uni-tuebingen.de>
2020-03-19 19:12:25 +0100

    nest models in C++

--- setup.sh
+++ setup.sh
@@ -14,11 +14,11 @@ sudo apt install cmake
 sudo apt install python3-pip
 alias python=python3
 alias pip=pip3
-pip install numpy,scipy,ipython, seaborn,networkx,h5py,cython
+pip install numpy,scipy,ipython, seaborn,networkx,h5py,cython,html
 cd /home/$USER
 git clone  https://github.com/nest/nestml.git
 cd /home/$USER/nestml
-sudo python -m pip install -r requirements.txt
-sudo python setup.py install
+sudo python3 -m pip install -r requirements.txt
+sudo python3 setup.py install
 #echo "export LD_LIBRARY_PATH=/home/$USER/.local/lib/" >> ~/.bashrc
 pip install pygsl
