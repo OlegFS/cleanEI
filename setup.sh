cc4d82fcee5c9d3f25f2b87b978d5e92eab77f8e
Oleg Vinogradov <ovinogradov@fenrir.mvl6.uni-tuebingen.de>
2020-03-19 17:45:05 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -4,13 +4,21 @@
 sudo add-apt-repository ppa:nest-simulator/nest
 sudo apt-get update
 sudo apt-get install nest
+sudo apt-get install libssl-dev
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null |
+    sudo apt-key add -
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt update
+sudo apt install cmake
+#sudo apt install cmake ubnutu has an old cmake
 sudo apt install python3-pip
+alias python=python3
+alias pip=pip3
+pip install numpy,scipy,ipython, seaborn,networkx,h5py,cython
 cd /home/$USER
 git clone  https://github.com/nest/nestml.git
 cd /home/$USER/nestml
-sudo python3 -m pip install -r requirements.txt
-sudo python3 setup.py install
-echo "export LD_LIBRARY_PATH=/home/$USER/.local/lib/" >> ~/.bashrc
-alias python=python3
-alias pip=pip3
+sudo python -m pip install -r requirements.txt
+sudo python setup.py install
+#echo "export LD_LIBRARY_PATH=/home/$USER/.local/lib/" >> ~/.bashrc
 pip install pygsl
