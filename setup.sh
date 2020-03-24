8a9b095405b418636f9cb650475ab34cdc86603f
Oleg Vinogradov <ovinogradov@fenrir.mvl6.uni-tuebingen.de>
2020-03-24 15:20:31 +0100

    fixed setup and added data

--- setup.sh
+++ setup.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 #Bash strartup
 #installs nest,nestml,pip3
-sudo add-apt-repository ppa:nest-simulator/nest
 sudo apt-get update
-sudo apt-get install nest
 sudo apt-get install libssl-dev
+#Update Cmake
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null |
     sudo apt-key add -
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 sudo apt update
 sudo apt install cmake
-#sudo apt install cmake ubnutu has an old cmake
+# Install python
 sudo apt install python3-pip
 alias python=python3
 alias pip=pip3
-pip install numpy,scipy,ipython, seaborn,networkx,h5py,cython,html
-cd /home/$USER
-git clone  https://github.com/nest/nestml.git
-cd /home/$USER/nestml
-sudo python3 -m pip install -r requirements.txt
-sudo python3 setup.py install
-#echo "export LD_LIBRARY_PATH=/home/$USER/.local/lib/" >> ~/.bashrc
+#Make sure the default python is python3 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+sudo python3 -m pip install ipython\
+	numpy\
+	scipy\
+	sortedcontainers\ 
+	matplotlib\
+	seaborn\
+	cython\
+	networkx\
+	wheel\
+	nose\
+	numba\
+	h5py
+sudo apt-get install -y build-essential\
+    cmake libltdl7-dev libreadline6-dev \
+    libncurses5-dev libgsl0-dev libtool-bin\
+    openmpi-bin libopenmpi-dev
+
+#Install NEST
 pip install pygsl
+mkdir nest-build &&\
+mkdir nest-install
+
+git clone  https://github.com/nest/nest-simulator.git  -b v2.20.0
+cd /home/$USER/nest-build
+cmake  -DCMAKE_INSTALL_PREFIX:PATH=/home/$USER/.local /home/$USER/nest-simulator
+make -j 18
+make install
+sudo echo "source /home/$USER/.local/bin/nest_vars.sh" >> ~/.bashrc
+sudo echo "export LD_LIBRARY_PATH=/home/$USER/.local/lib/" >> ~/.bashrc
+
+#Install MyModel (adLIF)
+cd /home/$USER/cleanEI/nestmodels/
+cmake -Dwith-nest=/home/$USER/.local/bin/nest-config .
+make
+make install
+#echo "export LD_LIBRARY_PATH=/home/$USER/.local/lib/" >> ~/.bashrc
